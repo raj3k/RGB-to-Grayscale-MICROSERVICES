cb31c599e4a1e38c1055b60d779d18342987896c
raj3k <rajkosky@gmail.com>
2023-01-15 21:32:45 +0100

    login page completed

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -3,19 +3,19 @@ import axios, { AxiosResponse } from 'axios';
 import { useSignIn } from 'react-auth-kit';
 import { useNavigate } from 'react-router-dom';
 
-type APIResponse = {
+type AuthResponse = {
     status: string,
     token: string
 }
 
-const Login = () => {
+export default function Login() {
     const signIn = useSignIn();
     const navigate = useNavigate();
     const [formData, setFormData] = useState({email: "", password: ""})
     const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         await axios.post('http://localhost:3050/api/gateway/login/', formData)
-            .then((res: AxiosResponse<APIResponse>) => {
+            .then((res: AxiosResponse<AuthResponse>) => {
                 if (res.status === 200) {
                     if (signIn(
                         {
@@ -32,13 +32,60 @@ const Login = () => {
     }
 
     return (
-        <form onSubmit={onSubmit}>
-            <input type={"email"} onChange={(e) => setFormData({...formData, email: e.target.value})} />
-            <input type={"password"} onChange={(e) => setFormData({...formData, password: e.target.value})} />
+        <>
+            <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+                <div className="w-full max-w-md space-y-8">
+                    <div>
+                        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                            Sign in to your account
+                        </h2>
+                    </div>
+                    <form className="mt-8 space-y-6" method="POST" onSubmit={onSubmit}>
+                        <input type="hidden" name="remember" defaultValue="true" />
+                        <div className="-space-y-px rounded-md shadow-sm">
+                            <div>
+                                <label htmlFor="email-address" className="sr-only">
+                                    Email address
+                                </label>
+                                <input
+                                    id="email-address"
+                                    name="email"
+                                    type="email"
+                                    autoComplete="email"
+                                    required
+                                    className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                    placeholder="Email address"
+                                    onChange={(e) => setFormData({...formData, email: e.target.value})}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="sr-only">
+                                    Password
+                                </label>
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                    placeholder="Password"
+                                    onChange={(e) => setFormData({...formData, password: e.target.value})}
+                                />
+                            </div>
+                        </div>
 
-            <button>Submit</button>
-        </form>
-    );
-};
-
-export default Login;
+                        <div>
+                            <button
+                                type="submit"
+                                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            >
+                                Sign in
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+}
